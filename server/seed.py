778fe8b00f797f50769ee66a3163c0c747ef2dc8
brewchetta <chett.tiller@gmail.com>
2024-12-04 09:42:57 -0500

    fixed seed file

--- server/seed.py
+++ server/seed.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
         Deli.query.delete()
         Hamburger.query.delete()
 
-        d1 = Deli(name="Mr Mango", location="Fort Greene", has_ice_cream=True, bacon_egg_cheese=True)
-        d2 = Deli(name="Mr Kiwi", location="Bed Stuy", has_ice_cream=True, bacon_egg_cheese=True)
-        d3 = Deli(name="Mr Pineapple", location="Park Slope", has_ice_cream=True, bacon_egg_cheese=True)
+        d1 = Deli(name="Mr Mango", location="Fort Greene", has_bodega_cat=True)
+        d2 = Deli(name="Mr Kiwi", location="Bed Stuy", has_bodega_cat=True)
+        d3 = Deli(name="Mr Pineapple", location="Park Slope", has_bodega_cat=True)
 
         db.session.add_all( [d1,d2,d3] )
         db.session.commit()
