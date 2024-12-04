365d81f159eb8240fda9900e094c253f0c431dc9
brewchetta <chett.tiller@gmail.com>
2024-12-04 10:50:20 -0500

    delis and burgers and pizzas oh my

--- server/models.py
+++ server/models.py
@@ -15,9 +15,53 @@ class Deli(db.Model):
     name = db.Column(db.String)
     has_bodega_cat = db.Column(db.Boolean)
 
+    hamburgers = db.relationship('Hamburger', back_populates='deli')
+    pizzas = db.relationship('Pizza', back_populates='deli')
+
 class Hamburger(db.Model):
 
     __tablename__ = "hamburgers_table"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+
+    deli_id = db.Column(db.Integer, db.ForeignKey('delis_table.id'))
+
+    deli = db.relationship('Deli', back_populates='hamburgers')
+
+class Pizza(db.Model):
+
+    __tablename__ = "pizza_table"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    deli_id = db.Column(db.Integer, db.ForeignKey('delis_table.id'))
+
+    deli = db.relationship('Deli', back_populates='pizzas')
+
+# THIS STUFF IS JUST FOR SEBASTIEN
+
+# class DeliveryTruck(db.Model):
+
+#     __tablename__ = "delivery_truck_table"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+    
+#     deliveries = db.relationship('Delivery', back_populates='delivery_truck')
+
+#     delis = association_proxy('deliveries', 'deli')
+
+# class Delivery(db.Model):
+
+#     __tablename__
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     date = db.Column
+
+#     deli_id
+#     delivery_truck_id
+
+#     deli = db.relationship
+#     delivery_truck = db.relationship('DeliveryTruck', back_populates='deliveries')
--- server/seed.py
+++ server/seed.py
@@ -15,4 +15,12 @@ if __name__ == '__main__':
         db.session.add_all( [d1,d2,d3] )
         db.session.commit()
 
+        h1 = Hamburger(name="Rodeo Burger", deli_id=d1.id)
+        h2 = Hamburger(name="Hulk Smash Burger", deli_id=d1.id)
+        h3 = Hamburger(name="Veggie Burger", deli_id=d2.id)
+        h4 = Hamburger(name="Derrick Zoolander's Burger for Children Who Don't Eat Very Good", deli_id=d3.id)
+
+        db.session.add_all( [h1,h2,h3,h4] )
+        db.session.commit()
+
         print("Seeding complete!")
